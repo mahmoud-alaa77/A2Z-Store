81f15429d0255a1c27db66c0a9ce1db27f90b140
Mahmoud Alaa <126196219+mahmoud-alaa77@users.noreply.github.com>
2024-07-03 13:28:34 +0300

    refactor code

--- lib/features/on_boarding/ui/on_boarding_screen.dart
+++ lib/features/on_boarding/ui/on_boarding_screen.dart
@@ -12,62 +12,65 @@ class OnBoardingScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: OnBoardingSlider(
-          skipIcon: const Icon(
-            Icons.arrow_forward,
-            color: Colors.white,
-          ),
-          finishButtonText: "Get Started",
-          finishButtonStyle: const FinishButtonStyle(
-              backgroundColor: AppColors.mainBlack,
-              shape: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(16)))),
-          headerBackgroundColor: Colors.white,
-          background: [
-            Container(
+        body: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8),
+          child: OnBoardingSlider(
+            skipIcon: const Icon(
+              Icons.arrow_forward,
               color: Colors.white,
             ),
-            Container(
-              color: Colors.white,
+            finishButtonText: "Get Started",
+            finishButtonStyle: const FinishButtonStyle(
+                backgroundColor: AppColors.mainBlack,
+                shape: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(16)))),
+            headerBackgroundColor: Colors.white,
+            background: [
+              Container(
+                color: Colors.white,
+              ),
+              Container(
+                color: Colors.white,
+              ),
+              Container(
+                color: Colors.white,
+              ),
+            ],
+            totalPage: 3,
+            speed: 1.8,
+            pageBodies: const [
+              OnBoardingPageBody(
+                title: "20% Discount\nNew Arrival Product ",
+                desc:
+                    "Publish up your selfies to make yourself\nmore beautiful with this app.",
+                imageUrl: "assets/images/on_boarding_image_1.jpg",
+              ),
+              OnBoardingPageBody(
+                title: "Take Advantage\nOf The Offer Shopping",
+                desc:
+                    "Publish up your selfies to make yourself\nmore beautiful with this app.",
+                imageUrl: "assets/images/on_boarding_image_2.jpg",
+              ),
+              OnBoardingPageBody(
+                title: "All Types Offers\nWithin Your Reach",
+                desc:
+                    "Publish up your selfies to make yourself\nmore beautiful with this app.",
+                imageUrl: "assets/images/on_boarding_image_3.jpg",
+              ),
+            ],
+            skipTextButton: Container(
+              width: 80.w,
+              //height: 70.h,
+              decoration: BoxDecoration(
+                color: AppColors.mainBlack,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: const Center(
+                  child: Text(
+                "Skip",
+                style: AppTextStyles.font16WhiteW700,
+              )),
             ),
-            Container(
-              color: Colors.white,
-            ),
-          ],
-          totalPage: 3,
-          speed: 1.8,
-          pageBodies: const [
-            OnBoardingPageBody(
-              title: "20% Discount\nNew Arrival Product ",
-              desc:
-                  "Publish up your selfies to make yourself\nmore beautiful with this app.",
-              imageUrl: "assets/images/on_boarding_image_1.jpg",
-            ),
-            OnBoardingPageBody(
-              title: "Take Advantage\nOf The Offer Shopping",
-              desc:
-                  "Publish up your selfies to make yourself\nmore beautiful with this app.",
-              imageUrl: "assets/images/on_boarding_image_2.jpg",
-            ),
-            OnBoardingPageBody(
-              title: "All Types Offers\nWithin Your Reach",
-              desc:
-                  "Publish up your selfies to make yourself\nmore beautiful with this app.",
-              imageUrl: "assets/images/on_boarding_image_3.jpg",
-            ),
-          ],
-          skipTextButton: Container(
-            width: 80.w,
-            //height: 70.h,
-            decoration: BoxDecoration(
-              color: AppColors.mainBlack,
-              borderRadius: BorderRadius.circular(12),
-            ),
-            child: const Center(
-                child: Text(
-              "Skip",
-              style: AppTextStyles.font16WhiteW700,
-            )),
           ),
         ),
       ),
