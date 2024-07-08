4ebe292ac5b633778063a2d8ec0b717fa6af6e02
Mahmoud Alaa <126196219+mahmoud-alaa77@users.noreply.github.com>
2024-07-08 13:30:18 +0300

    .

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,10 +7,8 @@ import Foundation
 
 import firebase_auth
 import firebase_core
-import rive_common
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-  RivePlugin.register(with: registry.registrar(forPlugin: "RivePlugin"))
 }
